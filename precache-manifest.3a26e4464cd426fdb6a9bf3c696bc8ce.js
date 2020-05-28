110b06cd6932294b67208973063d2ca69cf7ccd4
sellalone <imsalehmuhammad@gmail.com>
2020-05-28 18:37:53 +0200

    Updates

---
+++ precache-manifest.3a26e4464cd426fdb6a9bf3c696bc8ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e29db03c7e61842e6119912980d0d7",
+    "url": "/react-native-web-test/index.html"
+  },
+  {
+    "revision": "79b333f072abb56db9da",
+    "url": "/react-native-web-test/static/js/2.82f656ce.chunk.js"
+  },
+  {
+    "revision": "fc122c18e9c988dcd6c3fb24a73992f4",
+    "url": "/react-native-web-test/static/js/2.82f656ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a6730e0842b2d4c50d",
+    "url": "/react-native-web-test/static/js/main.f3c68cbf.chunk.js"
+  },
+  {
+    "revision": "ab955d2066e5c3539fec",
+    "url": "/react-native-web-test/static/js/runtime-main.8bd29778.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-native-web-test/precache-manifest.3a26e4464cd426fdb6a9bf3c696bc8ce.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-native-web-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
